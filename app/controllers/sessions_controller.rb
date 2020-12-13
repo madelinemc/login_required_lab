79b839379d5c88b62c2e7ab5cf023d0e7661252f
madelinemc <madeline.coons@gmail.com>
2020-12-13 17:16:53 -0600

    commit tree 52f99169af79f2dd390321a64bd4823d56b5ddff

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        if params[:name].nil? || params[:name].empty?
+            redirect_to '/'
+        else
+            session[:name] = params[:name]
+            redirect_to '/'
+        end
+    end
+
+    def destroy
+        if session[:name]
+            session.delete :name
+        end
+        redirect_to :login
+    end
+
+end
